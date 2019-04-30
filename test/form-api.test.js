5bf7fc94d8df2d9343278469c3eb100838fb0e9c
aramirezdecruz3148 <leigh3148@gmail.com>
2019-04-30 15:56:42 -0700

    I set up my new testing files and imported/exported accordingly, also set up my new TDD test ready to populate it

---
+++ test/form-api.test.js
@@ -0,0 +1,12 @@
+import formApi from '../src/form-api.js';
+const test = QUnit.test;
+QUnit.module('applicant api');
+
+
+
+test('round-trip applicant data', (assert) => {
+    // arrange
+    // act
+    // assert
+    assert.deepEqual(result, applicant);
+});
---
+++ test/get-form.test.js
@@ -0,0 +1,34 @@
+import getForm from '../src/get-form.js';
+const test = QUnit.test;
+
+QUnit.module('creating applicant form data');
+
+test('creates applicant from form data', (assert) => {
+    //Arrange
+    // Set up your parameters and expectations
+    const expected = {
+        name: 'Alex',
+        registryReason: ['I am new to the country'],
+        lawfullyObtained: true,
+        wandCore: 'dragon heart string',
+        wandWood: 'beech',
+        wandLength: 12,
+        uniqueWandTraits: 'My wand is fancy as hell.'
+    };
+
+    //calling in my form data that I am using
+    const formData = new FormData();
+    formData.set('name', expected.name);
+    formData.set('registry-reason', expected.registryReason);
+    formData.set('lawfully-obtained', 'yes');
+    formData.set('wand-core', expected.wandCore);
+    formData.set('wand-wood', expected.wandWood);
+    formData.set('wand-length', expected.wandLength);
+    formData.set('unique-wand-traits', expected.uniqueWandTraits);
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const form = getForm(formData);
+
+    //Assert
+    assert.deepEqual(form, expected);
+});
