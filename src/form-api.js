2e8168b7f2a65cd5c8638722a558f4173f4984b7
aramirezdecruz3148 <leigh3148@gmail.com>
2019-04-30 16:20:12 -0700

    I moved my new formAPI utility object into a new .js module and exported/imported accordingly

--- src/form-api.js
+++ src/form-api.js
@@ -1,4 +1,18 @@
-
-
+const formApi = {
+    save(form) {
+    //serialize to JSON
+        const json = JSON.stringify(form);
+    //save to local storage
+        localStorage.setItem('form', json);
+    },
+    get() {
+    //get from local storage
+        const json = localStorage.getItem('form');
+    //deserialize to object
+        const form = JSON.parse(json);
+    //return it
+        return form;
+    }
+};
 
 export default formApi;
--- test/form-api.test.js
+++ test/form-api.test.js
@@ -1,23 +1,7 @@
-// import formApi from '../src/form-api.js';
+import formApi from '../src/form-api.js';
 const test = QUnit.test;
 QUnit.module('form api');
 
-const formApi = {
-    save(form) {
-    //serialize to JSON
-        const json = JSON.stringify(form);
-    //save to local storage
-        localStorage.setItem('form', json);
-    },
-    get() {
-    //get from local storage
-        const json = localStorage.getItem('form');
-    //deserialize to object
-        const form = JSON.parse(json);
-    //return it
-        return form;
-    }
-};
 
 test('round-trip form data', (assert) => {
     // arrange
