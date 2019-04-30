010676bf3db9a3bfaf82aa7d0de8980986d5ea92
aramirezdecruz3148 <leigh3148@gmail.com>
2019-04-29 20:46:17 -0700

    I successfully TDD-ed my function, tomorrow I will export/import the function and finish calling it within my event listener

--- test/tests.js
+++ test/tests.js
@@ -1,15 +1,48 @@
 const test = QUnit.test;
 
+function getApplicant(formData) {
+    const lawfullyObtained = formData.get('lawfully-obtained') === 'yes';
+    const wandLength = parseInt(formData.get('wand-length'));
+
+    //make object literal
+    const applicant = {
+        name: formData.get('name'),
+        registryReason: formData.getAll('registry-reason'),
+        lawfullyObtained: lawfullyObtained,
+        wandCore: formData.get('wand-core'),
+        wandWood: formData.get('wand-wood'),
+        wandLength: wandLength,
+        uniqueWandTraits: formData.get('unique-wand-traits')
+    };
+    return applicant;
+}
+
+
 test('creates applicant from form data', (assert) => {
     //Arrange
     // Set up your parameters and expectations
-    const expected = {};
+    const expected = {
+        name: 'Alex',
+        registryReason: ['I am new to the country'],
+        lawfullyObtained: true,
+        wandCore: 'dragon heart string',
+        wandWood: 'beech',
+        wandLength: 12,
+        uniqueWandTraits: 'My wand is fancy as hell.'
+    };
 
     const formData = new FormData();
+    formData.set('name', expected.name);
+    formData.set('registry-reason', expected.registryReason);
+    formData.set('lawfully-obtained', 'yes');
+    formData.set('wand-core', expected.wandCore);
+    formData.set('wand-wood', expected.wandWood);
+    formData.set('wand-length', expected.wandLength);
+    formData.set('unique-wand-traits', expected.uniqueWandTraits);
     //Act 
     // Call the function you're testing and set the result to a const
     const applicant = getApplicant(formData);
-    
+
     //Assert
     assert.deepEqual(applicant, expected);
 });
