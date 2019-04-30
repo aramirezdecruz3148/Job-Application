215f6103320756e2a403d9e4d6e622f1eeff3935
aramirezdecruz3148 <leigh3148@gmail.com>
2019-04-30 16:18:03 -0700

    I successfully TDD-ed my form-api, and I am commiting before I move it to a new folder

--- test/form-api.test.js
+++ test/form-api.test.js
@@ -1,12 +1,30 @@
-import formApi from '../src/form-api.js';
+// import formApi from '../src/form-api.js';
 const test = QUnit.test;
-QUnit.module('applicant api');
+QUnit.module('form api');
 
+const formApi = {
+    save(form) {
+    //serialize to JSON
+        const json = JSON.stringify(form);
+    //save to local storage
+        localStorage.setItem('form', json);
+    },
+    get() {
+    //get from local storage
+        const json = localStorage.getItem('form');
+    //deserialize to object
+        const form = JSON.parse(json);
+    //return it
+        return form;
+    }
+};
 
-
-test('round-trip applicant data', (assert) => {
+test('round-trip form data', (assert) => {
     // arrange
+    const form = { name: 'spongebob' };
     // act
+    formApi.save(form);
+    const result = formApi.get();
     // assert
-    assert.deepEqual(result, applicant);
+    assert.deepEqual(result, form);
 });
