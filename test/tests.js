8488899bfed2feeaabf0e3f2a1cc8430246bbfc7
aramirezdecruz3148 <leigh3148@gmail.com>
2019-04-29 20:17:26 -0700

    set up my testing.js file

--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,15 @@
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+test('creates applicant from form data', (assert) => {
     //Arrange
     // Set up your parameters and expectations
+    const expected = {};
 
+    const formData = new FormData();
     //Act 
     // Call the function you're testing and set the result to a const
-
+    const applicant = getApplicant(formData);
+    
     //Assert
-    assert.equal(true, false);
+    assert.deepEqual(applicant, expected);
 });
