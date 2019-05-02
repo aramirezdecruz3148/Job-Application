328bd01b9a0d7a2447787b50a7f64bd196c4382d
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-02 08:09:17 -0700

    I took out my static table design, and psuedo coded my steps in my forms-details.js module

---
+++ src/forms-details.js
@@ -0,0 +1,9 @@
+//Importing my formAPI utility object
+
+//Reference the table body DOM element
+
+//Get the applicants from the API object
+
+//Loop the list of applicants
+
+//For each applicant, construct a table row and append to the table body
